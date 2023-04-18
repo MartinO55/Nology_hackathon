2c85bed9b525068c399138f5861102e4c76c1ef5
MacleanJS <macstaden@gmail.com>
2023-04-18 17:26:48 +1000

    graph

--- nologyhackathon/src/App.jsx
+++ nologyhackathon/src/App.jsx
@@ -3,15 +3,16 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import CodeWars from "./Mac/CodeWars";
+import CodewarsCont from "./Containers/CodewarsCont/CodewarsCont";
 
 function App() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <CodeWars username={"xNapha"} />
-    </div>
-  );
+    return (
+        <div className="App">
+            <CodewarsCont />
+        </div>
+    );
 }
 
 export default App;
--- nologyhackathon/src/Mac/CodeWars.jsx
+++ nologyhackathon/src/Mac/CodeWars.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import Plot from "react-plotly.js";
 
 const CodeWars = ({ username }) => {
     const [userData, setUserData] = useState(null);
     const [rankInfo, setRankInfo] = useState(null);
+    const [plotDataX, setPlotDataX] = useState(null);
+    const [plotDataY, setPlotDataY] = useState(null);
 
     const getData = async () => {
         const user = username;
@@ -14,25 +17,53 @@ const CodeWars = ({ username }) => {
         const data = await response.json();
         setUserData(data);
         console.log(data);
-        setRankInfo(data.ranks.overall);
+        setRankInfo(data.ranks);
+
+        const xArr = [];
+        const yArr = [];
+        Object.entries(data.ranks.languages).forEach((key) => {
+            xArr.push(key[0]);
+            yArr.push(key[1].score);
+        });
+        console.log(xArr, yArr);
+        const xArr2 = Object.values(xArr);
+        const yArr2 = Object.values(yArr);
+
+        setPlotDataX(xArr2);
+        setPlotDataY(yArr2);
     };
 
     useEffect(() => {
         getData();
-        // setRankInfo(userData.ranks.overall);
-        console.log(userData);
+        console.log("render");
     }, []);
+
+    console.log(plotDataX, "x data");
+    console.log(plotDataY, "y data");
 
     return userData ? (
         <div>
             <h3>{userData.username}</h3>
             <p>Clan: {userData.clan}</p>
             <p>Honor: {userData.honor}</p>
-            <p>Rank: {rankInfo.name}</p>
+            <p>Rank: {rankInfo.overall.name}</p>
             <p>Leaderboard Pos: {userData.leaderboardPosition}</p>
+            <Plot
+                data={[
+                    {
+                        x: plotDataX,
+                        y: plotDataY,
+                        type: "bar",
+                        mode: "lines+markers",
+                        marker: { color: "red" },
+                    },
+                    { type: "bar", x: [{ plotDataX }], y: [{ plotDataY }] },
+                ]}
+                layout={{ width: 320, height: 240, title: "A Fancy Plot" }}
+            />
         </div>
     ) : (
-        <p>"uhoh"</p>
+        <p>Rendering content</p>
     );
 };
 
